2c7d339b005af1c4c21401b3aaf972a32fefb583
madhura-avaloq <madhura.khandare@avaloq.com>
2025-01-03 16:27:26 +0530

    Updated main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,7 +1,7 @@
 provider "oci" {
   tenancy_ocid     = "ocid1.tenancy.oc1..aaaaaaaag3dw2b7t647frlr5ldhlajotfdn4j6zr6mljsau4kn3eqmriijcq"
   user_ocid        = "ocid1.user.oc1..aaaaaaaa5s255uspyi626hruf3mcfnvrzldwewwktrn6nku6lzvwgbi3a3pa"
-  fingerprint      = "f8:83:4b:2f:e8:fa:32:24:8e:69:22:13:00:19:67:fc"
-  private_key_path = "/home/opc/.oci/oci_api_key.pem"
+  fingerprint      = "85:7e:a0:d6:32:eb:25:01:3d:16:f0:d3:3f:9b:c1:c0"
+  private_key_path = "/home/opc/.oci/oci_project_key.pem"
   region           = "ap-mumbai-1"
 }
